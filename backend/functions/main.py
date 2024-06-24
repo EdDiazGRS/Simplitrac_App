ef2a3e03015e8be2741847d47347301396364a79
Ed Diaz <eddiaz@eds-macbook-air.mynetworksettings.com>
2024-06-24 18:42:31 -0400

    Temporary commit before pulling changes

--- backend/functions/main.py
+++ backend/functions/main.py
@@ -3,6 +3,11 @@ import os
 from firebase_functions import https_fn
 from pathlib import Path
 
+
+
 sys.path.insert(0, Path(__file__).parent.parent.parent.as_posix())
 from backend.functions.controllers.users_controller import create_new_user
+
+from backend.functions.controllers.ocr_controller import process_receipt
+
 
